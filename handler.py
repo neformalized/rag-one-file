20446c4211a48507a533bb2bdc40f1b5c772a396
neformalized <neformalized@gmail.com>
2026-01-05 18:50:05 +0200

    main

--- handler.py
+++ handler.py
@@ -9,7 +9,7 @@ class RAG:
         self.document_index = index.build(self.document_embedding)
     #
     
-    def query(self, query: str, top_k: int = 2, enhance: int = 5) -> str:
+    def query(self, query: str, top_k: int = 2, enhance: int = 3) -> str:
         
         queries_embedding = [vectorizer.vectorize(subquery, True) for subquery in llm.generate_search_queries(query, enhance)] if enhance > 0 else vectorizer.vectorize(query, True)
         
@@ -21,35 +21,9 @@ class RAG:
     #
 #
 
-
 """
-
-# Document prepare
-
 with open("C:\\Users\\omni\\desktop\\document.txt", "r", encoding="utf-8") as file:
-    
-    document = file.read()
+    store = RAG(document.read())
 #
-
-document_chunks = vectorizer.chunking(document, 500, 100)
-document_embedding = vectorizer.vectorize(document_chunks)
-document_index = index.build(document_embedding)
-
-# Query prepare
-
-query_origin = "ожидаемые результаты"
-#query_embedding = vectorizer.vectorize(query_origin, True)
-
-subquery_embedding = [vectorizer.vectorize(subquery, True) for subquery in llm.generate_search_queries(query_origin)]
-
-# Search
-
-#results = index.search(document_index, document_chunks, query_embedding, 2)
-
-results = retriever.dedup([index.search(document_index, document_chunks, query_embedding, 2) for query_embedding in subquery_embedding])
-
-# Join results
-
-print(llm.generate_answer(query_origin, results))
-
+result = store.query("ожидаемые результаты"))
 """
