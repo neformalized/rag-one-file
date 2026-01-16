670afa5f4b9db798e1e8d1ad8227f232928f9016
neformalized <neformalized@gmail.com>
2026-01-16 14:34:50 +0200

    main

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-from engine import rag
+from rag import RAG
 
 with open("C:\\Users\\omni\\Desktop\\document.txt", "r", encoding = "utf-8") as f:
     
     doc = f.read()
     
-    store = rag.RAG(doc)
+    store = RAG(doc)
 #
 
 print(store.query("Ожидания от искуственного интеллекта", top_k = 3, enhance = 5))
